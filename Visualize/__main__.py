47a226dec40c4617c03fd11353b00f7d6a8bc4f7
Amin <31763634+Amin-Matola@users.noreply.github.com>
2021-03-05 11:03:01 +0200

    Update __main__.py

--- Visualize/__main__.py
+++ Visualize/__main__.py
@@ -1,3 +1,13 @@
+"""
+This is __main__ file, the app will execute from here when the package is called on the console
+for instance:
+
+C:/Users/Example> python Visualize
+
+Will call this file, and the app will be booted
+"""
+
 from app import App
 
+# Boot the application from the console
 App.boot()
